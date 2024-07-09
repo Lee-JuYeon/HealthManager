a36331a84515b8556cd926f581521a125502df57
cavss <cavss1118@gmail.com>
2024-07-10 00:05:44 +0900

    NutritionModel init

---
+++ ios/HealthManager/HealthManager/Model/NutritionModel.swift
@@ -0,0 +1,22 @@
+//
+//  NutritionModel.swift
+//  HealthManager
+//
+//  Created by C.A.V.S.S on 7/10/24.
+//
+
+import SwiftUI
+
+struct NutritionModel : Hashable {
+    let calrories : Double // 탄수화물
+    let protein : Double // 단백질
+    let carbo : CarbohydrateModel // 탄수화물
+    let fat : FatModel // 지방
+    let sodium : Double // 나트륨
+    let cholesterol : Double // 콜레스테롤
+    let iron : Double // 철분
+    let calcium : Double // 칼슘
+    let potassium : Double // 칼륨
+    let vitamin : VitaminModel // 비타민
+    
+}
