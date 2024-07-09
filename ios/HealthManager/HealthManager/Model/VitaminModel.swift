daf0efee04ec6cb4be67c649526d2ab172302bb7
cavss <cavss1118@gmail.com>
2024-07-10 00:07:33 +0900

    VitaminModel init

---
+++ ios/HealthManager/HealthManager/Model/VitaminModel.swift
@@ -0,0 +1,25 @@
+//
+//  VitaminModel.swift
+//  HealthManager
+//
+//  Created by C.A.V.S.S on 7/10/24.
+//
+
+import SwiftUI
+
+struct VitaminModel : Hashable {
+    let vitaminA : Double // 비타민a
+    let vitaminB1 : Double
+    let vitaminB2 : Double
+    let vitaminB3 : Double
+    let vitaminB5 : Double
+    let vitaminB6 : Double
+    let vitaminB7 : Double
+    let vitaminB9 : Double
+    let vitaminB12 : Double
+    let vitaminC : Double
+    let vitaminD : Double
+    let vitaminE : Double
+    let vitaminK : Double
+}
+
--- ios/HealthManager/HealthManager/VM/DataVM.swift
+++ ios/HealthManager/HealthManager/VM/DataVM.swift
@@ -18,19 +18,3 @@ class DataVM: ObservableObject {
 
 
 
-
-struct VitaminModel : Hashable {
-    let vitaminA : Double // 비타민a
-    let vitaminB1 : Double
-    let vitaminB2 : Double
-    let vitaminB3 : Double
-    let vitaminB5 : Double
-    let vitaminB6 : Double
-    let vitaminB7 : Double
-    let vitaminB9 : Double
-    let vitaminB12 : Double
-    let vitaminC : Double
-    let vitaminD : Double
-    let vitaminE : Double
-    let vitaminK : Double
-}
