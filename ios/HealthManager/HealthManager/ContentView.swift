2c978d7e41e96faac5391089ffc74d8fc6978d41
cavss <cavss1118@gmail.com>
2024-07-09 19:08:48 +0900

    CustomTabView update

--- ios/HealthManager/HealthManager/ContentView.swift
+++ ios/HealthManager/HealthManager/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject private var screenVM : ScreenVM
+        
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        mainView()
+    }
+    
+    private func mainView() -> some View {
+        return CustomTabView(
+            setTabViewStyle: CustomTabView.CustomTabViewStyle.BottomNavigation,
+            setTabBackgroundColour: .backgroundColour,
+            setTabItemModels: [
+                CustomTabView.CustomTabItemModel(image: "image_up", title: "갓생"),
+                CustomTabView.CustomTabItemModel(image: "image_cards", title: "관리"),
+                CustomTabView.CustomTabItemModel(image: "image_profile", title: "프로필"),
+            ],
+            setSelectedIndex: $screenVM.pageIndex) { page in
+                switch(page) {
+                case 0 :
+                    GodLifeView()
+                case 1 :
+                    ManageView()
+                case 2 :
+                    SettingsView()
+                default :
+                    GodLifeView()
+                }
+            }
     }
 }
 
