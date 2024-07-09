350ad5734449f8a4fe5751ae18e6930b3eddb286
cavss <cavss1118@gmail.com>
2024-07-10 00:33:47 +0900

    text gradation

--- ios/HealthManager/HealthManager/UI/Screen/Nutrition/NutritionView.swift
+++ ios/HealthManager/HealthManager/UI/Screen/Nutrition/NutritionView.swift
@@ -10,11 +10,55 @@ import SwiftUI
 struct NutritionView : View {
     
     @EnvironmentObject private var dataVM : DataVM
+    
+    private func titleView() -> some View {
+        let titleText = "영양정보"
+        let colors: [Color] = [.blue, .green]
+        
+        return Group {
+            if #available(iOS 15, *) {
+                Text(titleText)
+                    .font(.system(size: 30, weight: .bold))
+                    .foregroundStyle(
+                        LinearGradient(
+                            colors: colors,
+                            startPoint: .leading,
+                            endPoint: .trailing
+                        )
+                    )
+            } else {
+                Text(titleText)
+                    .font(.system(size: 30, weight: .bold))
+                    .foregroundColor(.clear)
+                    .overlay(
+                        LinearGradient(
+                            colors: colors,
+                            startPoint: .leading,
+                            endPoint: .trailing
+                        )
+                        .mask(
+                            Text(titleText)
+                                .font(.system(size: 30, weight: .bold))
+                        )
+                    )
+            }
+        }
+    }
 
     //  영양성분 체크 (식단, 영양제)
     var body: some View {
         VStack(alignment: HorizontalAlignment.leading, spacing: 5){
+            HStack(alignment: VerticalAlignment.center, spacing: 5){
+                titleView()
+                
+                Image("globe")
+                    .resizable()
+                    
+            }
             
+            HStack(alignment: VerticalAlignment.center, spacing: 2){
+                
+            }
         }
     }
 }
